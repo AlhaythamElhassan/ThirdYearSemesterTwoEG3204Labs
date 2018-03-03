09b6f340d074f2f81cb0a76ae9da34f856a12c16
Alhaytham <asalhaytham@gmail.com>
2018-03-03 03:33:50 +0000

    add .gitignore

---
+++ programmable_4bit_Counter/programabl_4bits_counter.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity programable_4bits_counter is 
+	port (
+		);
+end programable_4bits_counter;
+
+architecture behavioural of programable_4bits_counter is 
+begin
+
+edn behavioural;
