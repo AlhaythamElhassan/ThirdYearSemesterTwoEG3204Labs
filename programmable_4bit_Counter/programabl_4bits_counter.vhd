539b6bb2448adba473758d8b42f13bd0c1a2d3ff
Alhaytham <asalhaytham@gmail.com>
2018-03-03 03:52:11 +0000

    create entity outline

--- programmable_4bit_Counter/programabl_4bits_counter.vhd
+++ programmable_4bit_Counter/programabl_4bits_counter.vhd
@@ -4,10 +4,11 @@ use ieee.std_logic_unsigned.all;
 
 entity programable_4bits_counter is 
 	port (
+			direction, enable_count, reset, clock : in std_logic
 		);
 end programable_4bits_counter;
 
 architecture behavioural of programable_4bits_counter is 
 begin
 
-edn behavioural;
+end behavioural;
