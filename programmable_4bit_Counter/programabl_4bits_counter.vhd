7af53fb3486a667ac1db235df95654863327736e
Alhaytham <asalhaytham@gmail.com>
2018-03-09 05:23:39 +0000

    add component Clock_Divider

--- programmable_4bit_Counter/programabl_4bits_counter.vhd
+++ programmable_4bit_Counter/programabl_4bits_counter.vhd
@@ -4,11 +4,25 @@ use ieee.std_logic_unsigned.all;
 
 entity programable_4bits_counter is 
 	port (
-			direction, enable_count, reset, clock : in std_logic
+			direction, enable_count, reset, clock : in std_logic;
+			mod_value :in std_logic_vector(3 downto 0);
+			 counter : out std_logic_vector(3 downto 0)
 		);
 end programable_4bits_counter;
 
 architecture behavioural of programable_4bits_counter is 
+
+-- component declaration
+
+component Clock_Divider is
+	GENERIC(LIMIT : integer := 2); 
+	PORT (
+			Clock	  : IN	STD_LOGIC;
+			Reset	  : IN	STD_LOGIC;
+			Clk_Div : OUT	STD_LOGIC
+		  );
+end component;
+
 begin
 
 end behavioural;
