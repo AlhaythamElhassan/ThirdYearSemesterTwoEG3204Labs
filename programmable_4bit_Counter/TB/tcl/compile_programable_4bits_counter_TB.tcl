d12c200acc98cb071890c316f903ce7bf2ca359a
Alhaytham <asalhaytham@gmail.com>
2018-03-11 02:08:20 +0000

    correct test bench

--- programmable_4bit_Counter/TB/tcl/compile_programable_4bits_counter_TB.tcl
+++ programmable_4bit_Counter/TB/tcl/compile_programable_4bits_counter_TB.tcl
@@ -6,9 +6,9 @@ vlib rtl_work
 vmap work rtl_work
 
 vcom -93 -work work {../../programable_4bits_counter.vhd}
-vcom -93 -work work {../../vhd/random_package.vhd}
+vcom -93 -work work {../../TB/vhd/random_package.vhd}
 
-vcom -93 -work work {../../programable_4bits_counter_TB.vhd}
+vcom -93 -work work {../../TB/vhd/programable_4bits_counter_TB.vhd}
 
 vsim -t 1ps -L altera -L lpm -L sgate -L altera_mf -L altera_lnsim -L cycloneiv_hssi -L cycloneiv_pcie_hip -L cycloneiv -L rtl_work -L work -voptargs="+acc"  programable_4bits_counter_TB
 
